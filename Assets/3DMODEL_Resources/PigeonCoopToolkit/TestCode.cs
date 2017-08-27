22e6d0648546f3c6810df3abd2682ccb05ffde87
SSQ\michaelssq <pangyarbo@gmail.com>
2017-08-27 10:14:20 +0800

    托尾特效

---
+++ Assets/3DMODEL_Resources/PigeonCoopToolkit/TestCode.cs
@@ -0,0 +1,27 @@
+﻿using PigeonCoopToolkit.Effects.Trails;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class TestCode : MonoBehaviour
+{
+
+    public List<TrailRenderer_Base> Trails;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Trails.ForEach(a => a.Emit = true);
+
+            transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane + 0.01f));
+
+        }
+        else
+        {
+            Trails.ForEach(a => a.Emit = false);
+        }
+    }
+
+}
