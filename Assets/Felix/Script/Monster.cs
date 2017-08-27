20e8fa259cef195d689db185e7cf60f2a44540af
Felix <rumsgr@gamil.com>
2017-08-27 11:16:06 +0800

    Felix: 角色更新

--- Assets/Felix/Script/Monster.cs
+++ Assets/Felix/Script/Monster.cs
@@ -6,20 +6,32 @@ using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
+
     public Animator animator;
     public GameObject target;
     public NavMeshAgent agent;
     public Vector3[] patrolPath;
     public float attackRange = 0.5f;
-    public float speed;
+    public float Speed;
+    public float attackTime;
 
-    private bool isPatrol;
+
+    private bool DoFuDeBuff;
+    private bool LiBaiDeBuff;
+    private bool WangWeiDeBuff;
+    public enum Types { NONE, ATTACK, PATROL, DONMOVE, RUNAWAY }
+    public Types type;
     private int patrolPoint = 1;
+    private float time;
+    private float donMoveTime = 1.5f;
 
     // Use this for initialization
     void Start()
     {
         agent.autoBraking = false;
+        agent.isStopped = true;
+        agent.speed = Speed;
+        type = Types.NONE;
     }
 
     public void GoToNextPoint()
@@ -33,31 +45,87 @@ public class Monster : MonoBehaviour
         agent.SetDestination(patrolPath[patrolPoint]);
         // 當巡邏一圈後會回到原來的點
         patrolPoint = (patrolPoint + 1) % patrolPath.Length;
-        isPatrol = true;
+        type = Types.PATROL;
         //agent.SetDestination(target.transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (type)
+        {
+            case Types.ATTACK:
+                CheckAttack();
+                break;
+            case Types.PATROL:
+                CheckPatrol();
+                break;
+            case Types.DONMOVE:
+                CheckNoMove();
+                break;
+        }
+
+        CheckDeBuff();
+    }
+
+    private void CheckAttack()
+    {
         if (target != null)
         {
             //如果能够得着了，就停下攻击
             if (agent.remainingDistance <= attackRange)
             {
-                agent.isStopped = true;
-                animator.SetBool("Attack", true);
+                if (time >= attackTime)
+                {
+                    agent.isStopped = true;
+                    animator.SetTrigger("Attack");
+                    time = 0;
+                }
+                time += Time.deltaTime;
             }
-        }
-        else if (isPatrol)
-        {
-
-            if (agent.remainingDistance <= 0.5f)
+            else
             {
-                agent.isStopped = true;
-                animator.SetBool("Walk", false);
-                isPatrol = false;
+                agent.speed = Speed * 1.5f;
+                agent.isStopped = false;
+                animator.SetBool("Run", true);
+                agent.SetDestination(target.transform.position);
             }
         }
+        else
+        {
+            agent.speed = Speed;
+            agent.isStopped = true;
+            animator.SetBool("Rum", false);
+        }
+    }
+
+    private void CheckPatrol()
+    {
+        if (agent.remainingDistance <= 0.5f)
+        {
+            agent.isStopped = true;
+            animator.SetBool("Walk", false);
+            type = Types.NONE;
+        }
+    }
+
+    public virtual void CheckNoMove()
+    {
+        if (time >= donMoveTime)
+        {
+            agent.isStopped = true;
+        }
+
+        time += Time.deltaTime;
+    }
+
+    public virtual void CheckDeBuff()
+    {
+
+    }
+
+    public void GetHit()
+    {
+        animator.SetTrigger("GetHit");
     }
 }
