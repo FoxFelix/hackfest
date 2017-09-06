1701af5affa98173a8faa7ed79ba1cc3358182ce
david <david.th.huang@gmail.com>
2017-09-06 17:35:48 +0800

    added sound to Krasue

---
+++ Assets/krasue_audioPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class krasue_audioPlayer : MonoBehaviour {
+
+    public AudioSource audioHead;
+    public AudioClip IdleSound;
+    public AudioClip AttackSound;
+    public AudioClip DamageSound;
+
+    public void PlayIdleAudio()
+    {
+        int i = Random.Range(0, 2);
+        if (i == 1)
+        {
+            if (!audioHead.isPlaying)
+                audioHead.PlayOneShot(IdleSound);
+        }
+    }
+
+    public void PlayAttackAudio()
+    {
+        if (audioHead.isPlaying)
+        {
+            audioHead.Stop();
+            audioHead.PlayOneShot(AttackSound);
+        }
+    }
+    public void PlayDamageAudio()
+    {
+        if (audioHead.isPlaying)
+        {
+            audioHead.Stop();
+            audioHead.PlayOneShot(DamageSound);
+        }
+    }
+}
